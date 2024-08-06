cdf6cd768c008166fb32eb709a04868556672413
Malav S Patel <malavpatel2607775@gmail.com>
2024-08-05 23:39:35 -0400

    Chapter-13 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,12 +25,16 @@ export async function createInvoice(formData: FormData) {
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
-  await sql`
+  try {
+    await sql`
         INSERT INTO invoices (customer_id, amount, status, date) 
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-
+  } catch (error) {
+    return {
+      message: 'Database Error : Failed to create Invoice',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -43,18 +47,25 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices 
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
     `;
-
-  revalidatePath('/dashboard/invoice');
+  } catch (error) {
+    return { message: 'Database error : Failed to update invoice' };
+  }
+  revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id =${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id =${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice' };
+  } catch (error) {
+    return { message: 'Database error : Failed to delete invoice' };
+  }
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,7 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  noStore()
+  noStore();
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
@@ -33,9 +33,9 @@ export async function fetchRevenue() {
     throw new Error('Failed to fetch revenue data.');
   }
 }
-  
+
 export async function fetchLatestInvoices() {
-  noStore()
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -44,7 +44,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices:any = data.rows.map((invoice) => ({
+    const latestInvoices: any = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
@@ -56,7 +56,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore()
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -98,7 +98,7 @@ export async function fetchFilteredInvoices(
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
-  noStore()
+  noStore();
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
@@ -129,7 +129,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore()
+  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -151,7 +151,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore()
+  noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -177,7 +177,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore()
+  noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -196,7 +196,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore()
+  noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -230,7 +230,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore()
+  noStore();
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
